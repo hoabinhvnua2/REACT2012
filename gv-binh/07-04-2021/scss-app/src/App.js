b96fe6e3f44231ac5f63fa7dc72f0320712958b3
= <tranhoabinh.tha.hua@gmail.com>
2021-04-07 21:56:11 +0700

    day 15

---
+++ gv-binh/07-04-2021/scss-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="ct -background">
+      <div className="ct__i1 -color">1</div>
+      <div className="container__item-2">2</div>
+    </div>
+
+    // sử dụng for và chuyển color của nhưng item ở vị trí 
+    // chẵn sang màu đỏ
+    // <div>
+    //   <p className="items-1">Angular</p>
+    //   <p className="items-2">React</p>
+    //   <p className="items-3">Vue</p>
+    //   <p className="items-4">React native</p>
+    //   <p className="items-5">Flutter</p>
+    // </div>
+  );
+}
+
+export default App;
